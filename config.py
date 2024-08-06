32d654488245f8e2bdee6d3bc13c9895bd816de6
sandeepowner07 <minhack984@gmail.com>
2024-08-06 13:41:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@
 BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("sandeepowner07")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("5588464519")
