5922980d13c987d027d55a816d431bac3d993875
sandeepowner07 <minhack984@gmail.com>
2024-08-08 10:20:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7424479555:AAG21En_vWhghzaUAhKjJ-DoZaLV3KBH4")
+BOT_TOKEN=("7291497669:AAGAEs4Y0MtPoHwH6fHDP-sxDOKrzXxkEls")
 
   #Enter Your telegram username here without @
 OWNER_USERNAME=("sandeepowner07")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("5269502580")
 
 
 
